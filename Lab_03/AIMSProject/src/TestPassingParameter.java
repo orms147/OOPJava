7b2c6b7a6a267b1162182d3067502db2ba72b1b4
Nguyen Ha Bach <orms147@gmail.com>
2024-11-26 02:57:58 +0700

    the exercise where you investigate on Java’s parameter passing

---
+++ Lab_03/AIMSProject/src/TestPassingParameter.java
@@ -0,0 +1,30 @@
+package AIMSProject.src;
+
+public class TestPassingParameter {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        DigitalVideoDisc[] dvds = {jungleDVD, cinderellaDVD};
+        swap(dvds);
+        System.out.println("jungle dvd title: " + dvds[0].getTitle());
+        System.out.println("cinderella dvd title: " + dvds[1].getTitle());
+
+        //changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+    }
+
+    public static void swap(DigitalVideoDisc[] dvds) {
+        String tmpTitle = dvds[0].getTitle();
+        dvds[0].setTitle(dvds[1].getTitle());
+        dvds[1].setTitle(tmpTitle);
+    }
+
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+        dvd = new DigitalVideoDisc(oldTitle);
+    }
+}
