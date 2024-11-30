e634b3dfa30d441d42234700c8f0a0ebbf065085
Nguyen Ha Bach <orms147@gmail.com>
2024-11-30 11:26:58 +0700

    add interface

--- AIMSProject/src/hust/soict/hedspi/aims/media/DigitalVideoDisc.java
+++ AIMSProject/src/hust/soict/hedspi/aims/media/DigitalVideoDisc.java
@@ -1,79 +1,26 @@
-package hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+package hust.soict.hedspi.aims.media;
 
-//import java.time.LocalDate;
-
-public class DigitalVideoDisc {
-  private static int nbDigitalVideoDiscs = 0;
-  private int id;
-  private String title;
-  private String category;
-  private String director;
-  private int length;
-  private double cost;
-  // private LocalDate addDate;
-  
-  public String getTitle() {
-    return title;
-  }
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getCategory() {
-    return category;
-  }
-  public String getDirector() {
-    return director;
-  }
-  public int getLength() {
-    return length;
-  }
-  public double getCost() {
-    return cost;
-  }
-  // public LocalDate getDate() {
-  //   return addDate;
-  // }
-  public int getId() {
-    return id;
-  }
-
-  public DigitalVideoDisc(String title, String category, double cost) {
-    this.title = title;
-    this.category = category;
-    this.cost = cost;
-    this.id = ++nbDigitalVideoDiscs;
-    //this.addDate = LocalDate.now();
-  }
-  public DigitalVideoDisc(String title) {
-    this.title = title;
-    this.id = ++nbDigitalVideoDiscs;
-    //this.addDate = LocalDate.now();
-  }
-  public DigitalVideoDisc(String title, String category, String director, double cost) {
-    this.title = title;
-    this.category = category;
-    this.director = director;
-    this.cost = cost;
-    this.id = ++nbDigitalVideoDiscs;
-    //this.addDate = LocalDate.now();
-  }
-  public DigitalVideoDisc(String title, String category, String director, int length, double cost) {
-    this.title = title;
-    this.category = category;
-    this.director = director;
-    this.length = length;
-    this.cost = cost;
-    this.id = ++nbDigitalVideoDiscs;
-    //this.addDate = LocalDate.now();
-  }
-
-  public String toString() {
-    return "DVD - " + this.getTitle() + " - " + 
-                      this.getCategory() + " - " + 
-                      this.getDirector() + " - " + 
-                      this.getLength() + ": " + 
-                      this.getCost() + " $";
-  }
-  
+public class DigitalVideoDisc extends Disc implements Playable {
+    
+    public DigitalVideoDisc() {
+        super();
+    }
+    
+    public DigitalVideoDisc(int id, String title, String category, float cost, int length, String director) {
+        super(id, title, category, cost, length, director);
+    }
+    
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this(0, title, category, cost, 0, null);
+    }
+    
+    public DigitalVideoDisc(String title) {
+        this(0, title, null, 0, 0, null);
+    }
+    
+    @Override
+    public void play() {
+        System.out.println("Playing DVD: " + this.getTitle());
+        System.out.println("DVD length: " + this.getLength());
+    }
 }
